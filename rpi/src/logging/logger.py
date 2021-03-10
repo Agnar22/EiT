05b646ab05d357c86d564c362ae3047052cf90be
Martin Falang <falang.martin@gmail.com>
2021-03-10 14:22:35 +0100

    Add comments

--- rpi/src/logging/logger.py
+++ rpi/src/logging/logger.py
@@ -6,15 +6,20 @@ class DataLogger():
     def __init__(self, data_path="../../data/"):
         self.data_path = data_path
 
+        self.measurements = ["g-force", "pos"]
         self.num_measurements = len(self.measurements)
 
-    def _pack(self, sensor_data: dict):
+    def log(self, sensor_data: dict):
+
+        # sensor_data format: 
+        # {"sensor_type": [value1, value2, value3..], "sensor_type2", [value1, value2, ...]}
+        
+
         log_entry = []
 
         for sensor, data in sensor_data.items():
             if sensor in self.measurements:
-
-                
+                # 
 
 
 
