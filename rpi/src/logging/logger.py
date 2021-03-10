15937d984755372868fcf3793c460c9604fe9c58
Agnar22 <agnar.bjornstad@gmail.com>
2021-03-10 14:51:19 +0100

    Fix writing of logs.

--- rpi/src/logging/logger.py
+++ rpi/src/logging/logger.py
@@ -1,5 +1,6 @@
+import datetime
+import pandas as pd
 
-import time
 
 class DataLogger():
 
@@ -10,26 +11,22 @@ class DataLogger():
         self.num_measurements = len(self.measurements)
 
     def log(self, sensor_data: dict):
-
-        # sensor_data format: 
+        # sensor_data format:
         # {"sensor_type": [value1, value2, value3..], "sensor_type2", [value1, value2, ...]}
         # Log NULL for values not present in measurements, e.g. if "pos" is not
         # present in sensor_data then logg a NULL for all its values
 
-        
-
-        log_entry = []
-
-        for sensor, data in sensor_data.items():
-            if sensor in self.measurements:
-                # 
-
-
+        df = pd.DataFrame.from_dict(sensor_data)
+        df = df.reindex(columns=self.measurements)
+        df.to_csv(f'{self.data_path}{datetime.datetime.now()}_{len(df.index)}', header=False, index=False,
+                  na_rep="null")
 
 
 if __name__ == "__main__":
     import os
-
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
+
+    dl = DataLogger()
+    dl.log({"pos": [1, 2, 3, 4, None], "g-force": [-1, -2, -3, -4, 1]})
